e81a3ce6476ab4436e5adcfbf112d5ee8a797790
dularion <antonia.hulha@systaro.de>
2015-08-16 00:12:39 +0200

    improved http code for unreg users

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -17,5 +17,5 @@ module.exports = function sessionAuth(req, res, next) {
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  return res.send(401, 'You need to be logged in to carry out this action.');
 };
